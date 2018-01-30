4e31373dd7587d45ed4245aaa6ccfe1e3538e5b9
lorenaquint <lorenaquint@gmail.com>
2018-01-29 23:04:30 -0500

    Cambios

---
+++ Propinas/Propinas/Propinas/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Propinas
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void BtnCalcular_OnClicked(Object sender, EventArgs e)
+            {
+            var total = (decimal.Parse(txtTotal.Text));
+            var propina = int.Parse(txtPropina.Text);
+            var numPersonas = int.Parse(txtPersonas.Text);
+            var totalPropina = (total * propina) / 100;
+            lbPropina.Detail = totalPropina.ToString("C");
+            lbTotal.Detail = (total + totalPropina).ToString("C");
+            lblPropinaPorPersona.Detail = (totalPropina / numPersonas).ToString("C");
+            lblTotalPorPersona.Detail =( (totalPropina + total) / numPersonas).ToString("C");
+
+            }
+	}
+}
